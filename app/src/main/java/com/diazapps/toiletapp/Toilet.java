1bfdb411017bfdc8a428a9abde426bcfc8fe7e86
deeq92 <deeq92@mail.com>
2017-08-04 01:44:26 -0700

    .

--- app/src/main/java/com/diazapps/toiletapp/Toilet.java
+++ app/src/main/java/com/diazapps/toiletapp/Toilet.java
@@ -2,8 +2,32 @@ package com.diazapps.toiletapp;
 
 public class Toilet {
 
-    public String location_name;
-    public String location_description;
-    public double rating;
+    private String location_name;
+    private String location_description;
+    private double rating;
 
+
+    public String getLocation_name() {
+        return location_name;
+    }
+
+    public void setLocation_name(String location_name) {
+        this.location_name = location_name;
+    }
+
+    public String getLocation_description() {
+        return location_description;
+    }
+
+    public void setLocation_description(String location_description) {
+        this.location_description = location_description;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
